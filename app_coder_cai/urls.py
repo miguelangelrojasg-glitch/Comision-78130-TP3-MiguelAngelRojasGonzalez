8b5334283595c05823eee053a346ae9ba7c5bd77
Miguel Angel Rojas Gonzalez <miguelangel.rojasg@gmail.com>
2025-11-01 13:36:35 -0300

    TPF  Vl

--- app_coder_cai/urls.py
+++ app_coder_cai/urls.py
@@ -7,11 +7,17 @@ urlpatterns = [
     path("test/", views.test, name="test"),
 
     path("clientes/", views.lista_cliente, name="lista_cliente"),
+    path("clientes/<int:pk>/eliminar", views.eliminar_cliente, name="eliminar_cliente"),
     path("clientes/nuevo/", views.nuevo_cliente, name="nuevo_cliente"),
+    path("clientes/modificar/<int:pk>", views.modificar_cliente, name="modificar_cliente"),
 
     path("productos/", views.lista_producto, name="lista_producto"),
+    path("productos/<int:pk>/eliminar", views.eliminar_producto, name="eliminar_producto"),
     path("productos/nuevo/", views.nuevo_producto, name="nuevo_producto"),
 
     path("compras/", views.lista_compra, name="lista_compra"),
     path("compras/nuevo/", views.nueva_compra, name="nueva_compra"),
+    path("compras/modificar/<int:pk>", views.modificar_compra, name="modificar_compra"),
+    path("compras/<int:pk>/eliminar", views.eliminar_compra, name="eliminar_compra"),
+
 ]
--- app_coder_cai/views.py
+++ app_coder_cai/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from app_coder_cai.forms import ClienteForm, ProductoForm, CompraForm
 from app_coder_cai.models import Cliente, Producto, Compra
-
+from django.contrib import messages
 
 def index(request):
     return render(request, "app_coder_cai/index.html")
@@ -34,6 +34,27 @@ def lista_cliente(request):
     return render(request, "app_coder_cai/cliente_list.html", {"cliente": cliente, "query": query})
 
 
+def eliminar_cliente(request, pk): # pk = ID en la base de datos
+    #cliente = Cliente.objects.get(nombre=pk)
+    cliente = get_object_or_404(Cliente, pk=pk)
+    cliente.delete()
+    messages.success(request, "Cliente eliminado correctamente")
+    return redirect("lista_cliente")
+
+
+def modificar_cliente(request, pk):  # ← agrega pk
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == "POST":
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cliente modificado correctamente.")
+            return redirect("lista_cliente")
+    else:
+        form = ClienteForm(instance=cliente)  # ← usar ClienteForm, no Cliente
+    
+    return render(request, "app_coder_cai/nuevo_cliente_form.html", {"form": form, "edicion": True})
+
 # -----------------------
 # PRODUCTOS
 # -----------------------
@@ -56,6 +77,13 @@ def nuevo_producto(request):
         form = ProductoForm()
     return render(request, "app_coder_cai/nuevo_producto_form.html", {"form": form})
 
+def eliminar_producto(request, pk): # pk = ID en la base de datos
+    #cliente = Cliente.objects.get(nombre=pk)
+    producto = get_object_or_404(Producto, pk=pk)
+    producto.delete()
+    messages.success(request, "Producto eliminado correctamente")
+    return redirect("lista_producto")
+
 
 # -----------------------
 # COMPRAS
@@ -74,3 +102,23 @@ def nueva_compra(request):
     else:
         form = CompraForm()
     return render(request, "app_coder_cai/nueva_compra_form.html", {"form": form})
+
+def eliminar_compra(request, pk): # pk = ID en la base de datos
+    #cliente = Cliente.objects.get(nombre=pk)
+    compra = get_object_or_404(Compra, pk=pk)
+    compra.delete()
+    messages.success(request, "Compra eliminada correctamente")
+    return redirect("lista_compra")
+
+def modificar_compra(request, pk):
+    compra = get_object_or_404(Compra, pk=pk)
+    if request.method == "POST":
+        form = CompraForm(request.POST, instance=compra)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Compra modificada correctamente.")
+            return redirect("lista_compra")
+    else:
+        form = CompraForm(instance=compra)
+
+    return render(request, "app_coder_cai/nueva_compra_form.html", {"form": form, "compra": compra, "modo": "editar"})
